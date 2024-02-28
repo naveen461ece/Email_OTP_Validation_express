3d4c60bb80faf7bc368afe38fcd9ded4fe9ceef7
Naveen G <124913987+naveen461ece@users.noreply.github.com>
2024-02-28 16:41:32 +0530

    Update mailer.js

--- mailer.js
+++ mailer.js
@@ -8,16 +8,16 @@ transporter = nodemailer.createTransport ( {
     secure:true, //use secure connection SSL 
     
     auth : {
-        user:'naveen461ece@gmail.com', //Gmail account username 
-        pass:'lzlv rwud lekb cjsq' //Gmail account app password
+        user:'yourmail@gmail.com', //Gmail account username 
+        pass:'give your password' //Gmail account app password
 
     }
 })
 
 //Define the email details and content 
 mail = {
-    from:'naveen461ece@gmail.com',
-    to:'naveen461ece@gmail.com',
+    from:'fromemail@gmail.com',
+    to:'toemail@gmail.com',
     subject:'OTP',
     text:`Hello user ,welcome to our ADIT Course!.your otp is ${genotp()}`
 }
